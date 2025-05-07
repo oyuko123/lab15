7ae190c437f9ea29fdd0f8adca8cd7177feb616b
Oyuko123 <oyunjargaltumenbayr139@gmail.com>
2025-05-07 22:36:47 +0800

    Initial commit

---
+++ src/main/java/lab01/sict/must/edu/mn/Calculator.java
@@ -0,0 +1,12 @@
+package lab01.sict.must.edu.mn;
+
+public class Calculator {
+
+    public double add(double a, double b) {
+        return a + b;
+    }
+
+    public double subtract(double a, double b) {
+        return a - b;
+    }
+}
---
+++ src/test/java/lab01/sict/must/edu/mn/CalculatorTest.java
@@ -0,0 +1,19 @@
+package lab01.sict.must.edu.mn;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class CalculatorTest {
+
+    Calculator calculator = new Calculator();
+
+    @Test
+    public void testAdd() {
+        assertEquals(5.0, calculator.add(2.0, 3.0));
+    }
+
+    @Test
+    public void testSubtract() {
+        assertEquals(1.0, calculator.subtract(3.0, 2.0));
+    }
+}
